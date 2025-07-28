926a87f59f8fdbae79ad7016f238300a4d967181
Yutae Lee <dobidugi@gmail.com>
2025-07-28 15:48:17 +0900

    add: spring-batch basic project

--- spring-batch/src/main/java/com/playground/springbatch/config/BatchConfig.java
+++ spring-batch/src/main/java/com/playground/springbatch/config/BatchConfig.java
@@ -1,30 +1,30 @@
-package com.playground.springbatch.config;
-
-import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class BatchConfig extends DefaultBatchConfiguration {
-
-    @Bean
-    public DataSource dataSource() {
-        System.out.println("Initializing H2 in-memory database for Spring Batch");
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("org/springframework/batch/core/schema-h2.sql")
-
-                .build();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-}
+//package com.playground.springbatch.config;
+//
+//import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class BatchConfig extends DefaultBatchConfiguration {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        System.out.println("Initializing H2 in-memory database for Spring Batch");
+//
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript("org/springframework/batch/core/schema-h2.sql")
+//                .build();
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        return new DataSourceTransactionManager(dataSource());
+//    }
+//}
--- spring-batch/src/main/java/com/playground/springbatch/config/SystemTerminationConfig.java
+++ spring-batch/src/main/java/com/playground/springbatch/config/SystemTerminationConfig.java
@@ -3,13 +3,11 @@ package com.playground.springbatch.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.concurrent.atomic.AtomicInteger;
