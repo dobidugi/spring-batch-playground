a279bd13daffca30bac6db08349f9b505fa8f475
arkytlee <dobidugi@gmail.com>
2025-10-29 17:54:34 +0900

    RDBMS에서 데이터를 읽고 쓰는 실습 작업 백업

---
+++ spring-batch-rdbms/src/main/java/com/playground/rdbms/SpringBatchRdbms.java
@@ -0,0 +1,14 @@
+package com.playground.rdbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBatchRdbms {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchRdbms.class, args);
+    }
+
+}
